14c56052d2be7b1f3b622d77abb00ada6ce37459
Ihor Naidonov <inaidonov@external.monom.ai>
2024-10-27 18:28:43 +0200

    fix: issues with pm2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
+require('dotenv').config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 4200);
