1e241ebdc3c134545b6ad91549b6f5e7b5044754
Ihor Naidonov <inaidonov@external.monom.ai>
2024-12-01 10:45:57 +0200

    refactoring: app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { Answer } from 'src/answer/entities/answer.entity';
 import { AppDataSource } from 'src/common/data-source/data-source';
-import { Question } from 'src/question/entities/question.entity';
-import { Result } from 'src/result/entities/result.entity';
-import { User } from 'src/user/entities/user.entity';
 import { TelegramModule } from 'src/telegram/telegram.module';
 import { AnswerModule } from 'src/answer/answer.module';
 import { ResultModule } from 'src/result/result.module';
@@ -16,7 +12,6 @@ import { UserModule } from 'src/user/user.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerModule } from 'src/scheduler/scheduler.module';
 import { WeeklyWinnerModule } from 'src/weekly-winner/weekly-winner.module';
-import { WeeklyWinner } from 'src/weekly-winner/entities/weekly-winner.entity';
 import { AppConfigModule } from 'src/app-config/app-config.module';
 
 @Module({
@@ -25,7 +20,6 @@ import { AppConfigModule } from 'src/app-config/app-config.module';
     TypeOrmModule.forRoot({
       ...AppDataSource.options,
     }),
-    TypeOrmModule.forFeature([Question, Answer, User, Result, WeeklyWinner]),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'assets'),
     }),
