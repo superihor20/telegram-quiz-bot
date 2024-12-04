563256ef57115a7875b17c01d4323e90898074a9
Ihor Naidonov <inaidonov@external.monom.ai>
2024-12-04 15:08:02 +0200

    fix: test

--- src/result/result.respository.ts
+++ src/result/result.respository.ts
@@ -146,7 +146,6 @@ export class ResultRepository extends Repository<Result> {
     startOfWeek: Date,
     endOfWeek: Date,
   ) {
-    console.log(123);
     const query = `
       WITH filtered_question_chat AS (
         SELECT DISTINCT
